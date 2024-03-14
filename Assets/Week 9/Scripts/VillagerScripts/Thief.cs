6d01e7d979b402c16010b63f08721eeb61381837
casiana-atki <atkicasi@sheridancollege.ca>
2024-03-14 14:45:47 -0400

    Week 9 Task 4A Progressing - Thief uses the Villager class, interacts with the proper chests and two knives spawn when the attack is done.

--- Assets/Week 9/Scripts/VillagerScripts/Thief.cs
+++ Assets/Week 9/Scripts/VillagerScripts/Thief.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class Thief : Villager
 {
 
+    public GameObject knifePrefab;
+    public Transform knifeSpawn1;
+    public Transform knifeSpawn2; 
+
+    public override ChestType CanOpen()
+    {
+        return ChestType.Thief;
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        Instantiate(knifePrefab, knifeSpawn1.position, knifeSpawn1.rotation);
+        Instantiate(knifePrefab, knifeSpawn2.position, knifeSpawn2.rotation); 
+    }
 }
